61c6df50b40fb62124a462b98680544c082aad3b
Sebastien Campa <Sebastien.Campa@iCloud.com>
2022-12-12 08:49:27 -0600

    mor estuff

--- Account.java
+++ Account.java
@@ -1,3 +1,41 @@
 public class Account extends Person {
+    static int uid;
+    private int id;
+    private boolean isPaying, hasTrainer;
+
+    public Account(String name, String phone, String email) {
+        super(name, phone, email);
+        id = uid;
+        uid++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isPaying() {
+        return isPaying;
+    }
+
+    public void setPaying(boolean isPaying) {
+        this.isPaying = isPaying;
+    }
+
+    public void pay(Trainer b) {
+        Payment a = new Payment(id);
+        a.addMembership(false, getTrainer(b));
+    }
+
+    public void getTrainer(Trainer a) {
+        hasTrainer = true;
+    }
+
+    public void cancelTrainer() {
+        hasTrainer = false;
+    }
     
 }
--- Payment.java
+++ Payment.java
@@ -1,3 +1,39 @@
-public class Payment extends Account {
-    
+public class Payment {
+    private boolean isMonthly;
+    private double rate;
+    private int id;
+
+    public Payment(int id) {
+        this.id = id;
+        this.isMonthly = false;
+        this.rate = 0;
+    }
+
+    public boolean isMonthly() {
+        return isMonthly;
+    }
+
+    public void setMonthly(boolean isMonthly) {
+        this.isMonthly = isMonthly;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    public boolean addMembership(boolean isMonthly, Trainer getTrainer) {
+        if (rate != 0 || isMonthly) {
+            return false;
+        }
+        rate = 30;
+        isMonthly = true;
+        if (getTrainer != null) {
+            rate += getTrainer.getRate();
+        }
+        return true;
+    }
 }
