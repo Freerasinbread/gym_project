26ca5569858a3d8838e491f66c369e294e5d55bf
Sebastien Campa <Sebastien.Campa@iCloud.com>
2022-12-13 08:46:49 -0600

    more stufff

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in); 
+    public static void main(String[] args) {
+        int ans = -1;
+        while (ans != 0) {
+            System.out.println("");
+
+            ans = sc.nextInt();
+            sc.nextLine();
+
+            if (ans == 1) {
+
+            }
+        }
+    }
+}
--- Schedule.java
+++ Schedule.java
@@ -1,3 +1,39 @@
+import java.util.ArrayList;
+
 public class Schedule{
-    
+    private ArrayList<String> days;
+    private double hoursPerDay;
+    private String timeStart, timeEnd;
+
+    public Schedule(ArrayList<String> days, double hpd, String start, String end) {
+        this.days = days;
+        hoursPerDay = hpd;
+        timeStart = start;
+        timeEnd = end;
+    }
+
+    public ArrayList<String> getDays() {
+        return days;
+    }
+
+    public double getHours() {
+        return hoursPerDay;
+    }
+
+    public String getTimes() {
+        return timeStart + " " + timeEnd;
+    }
+
+    public void setDays(ArrayList<String> days) {
+        this.days = days;
+    }
+
+    public void setHours(double hours) {
+        hoursPerDay = hours;
+    }
+
+    public void setTimes(String start, String end) {
+        timeStart = start;
+        timeEnd = end;
+    }
 }
